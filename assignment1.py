8f75759ecc520a39da3aaa67b29eb2bb8b328b5b
Liam Tolkkinen <liamtolkki@gmail.com>
2024-02-02 22:22:34 -0500

    Added the main function and started arg parsing

---
+++ assignment1.py
@@ -0,0 +1,18 @@
+# Liam Tolkkinen
+# CS 7200
+# Assignment 1 (Modified Gale-Shapley algorithm)
+# Final Draft due: 02/17/2024
+
+import sys
+
+def main():
+    # process file name:
+    if len(sys.argv) != 2:
+        print("make sure to include the file name of the input file!\n")
+        sys.exit(1)
+    inputFileName = sys.argv[1]
+    
+
+
+if __name__ == "__main__":
+    main() # execute main
