f0fa7ee30339350cc53bcc2fecdc8cd83ce3b3d8
Liam Tolkkinen <liamtolkki@gmail.com>
2024-02-02 23:13:40 -0500

    Added the person constructor

--- assignment1.py
+++ assignment1.py
@@ -6,6 +6,11 @@
 import sys
 import re
 
+class Person:
+    def __init__(self, gender, number):
+        
+
+
 # Returns a number found in the filename
 def getNumber(filename):
     digits = re.findall(r'\d+', filename) # Regex to find the digits in the name
@@ -16,13 +21,42 @@ def getNumber(filename):
 
 
 def main():
+    # default values, forward declaration
+    numberOfMen = 0
+    numberOfWomen = 0
     # process file name:
     if len(sys.argv) != 2:
         print("make sure to include the file name of the input file!\n")
         sys.exit(1)
-    inputFileName = sys.argv[1]
+    inputFileName = sys.argv[1] # argv[1] contains the second argument (filename)
     fileNumber = getNumber(inputFileName)
-    print("Filename: " + inputFileName + ", number: " + str(fileNumber))
+
+    # the next line was used for testing purposes:
+    # print("Filename: " + inputFileName + ", number: " + str(fileNumber))
+    
+    #this is used to filter out the '0' if needed
+    if fileNumber > 0: #if greater than 0
+        digit = str(fileNumber)
+    else: #if equal to 0
+        digit = ""
+
+    # for making the output file
+    outputFileName = "output" + digit + ".txt" 
+
+    with open(inputFileName, 'r') as file:
+        firstLine = file.readline()
+        numbers = firstLine.split()
+        if len(numbers) == 2: # should always be 2!
+            numberOfMen = int(numbers[0])
+            numberOfWomen = int(numbers[1])
+        
+        for i in range(numberOfMen):
+
+        for i in range(numberOfWomen):
+
+
+
+
     
 
 if __name__ == "__main__":
