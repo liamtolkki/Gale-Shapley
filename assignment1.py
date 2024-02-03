139c679be1eee751e2f0afe68dc1638b29ed1a46
Liam Tolkkinen <liamtolkki@gmail.com>
2024-02-02 22:35:53 -0500

    Working number parser

--- assignment1.py
+++ assignment1.py
@@ -4,6 +4,16 @@
 # Final Draft due: 02/17/2024
 
 import sys
+import re
+
+# Returns a number found in the filename
+def getNumber(filename):
+    digits = re.findall(r'\d+', filename) # Regex to find the digits in the name
+    if not digits:
+        return 0
+    number = int(''.join(digits))
+    return number
+
 
 def main():
     # process file name:
@@ -11,8 +21,9 @@ def main():
         print("make sure to include the file name of the input file!\n")
         sys.exit(1)
     inputFileName = sys.argv[1]
+    fileNumber = getNumber(inputFileName)
+    print("Filename: " + inputFileName + ", number: " + str(fileNumber))
     
-
 
 if __name__ == "__main__":
     main() # execute main
