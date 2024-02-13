89b2bc70711d2d3f8666c7baa46399e626e9d6b2
Liam Tolkkinen <liamtolkki@gmail.com>
2024-02-13 14:05:13 -0500

    Added a py script to generate preference files

---
+++ GS-Input-Generator.py
@@ -0,0 +1,11 @@
+#This program is solely intended for making input files
+#for assignment1.py
+#it will output a random preference listing 
+#for a random number of men and women
+
+def main():
+    menCount = int(input("How many men?"))
+    womenCount = int(input("How many women?"))
+
+if __name__ == "_main_":
+    main()
